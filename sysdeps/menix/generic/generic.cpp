47a0368e9a6d2f4a60e4d18ffdf1472d35d145fb
Marvin Friedrich <contact@marvinf.com>
2025-02-21 01:58:36 +0100

    New syscalls

--- sysdeps/menix/generic/generic.cpp
+++ sysdeps/menix/generic/generic.cpp
@@ -1,213 +1,191 @@
 #include <bits/ensure.h>
+#include <dirent.h>
+#include <errno.h>
 #include <mlibc/all-sysdeps.hpp>
 #include <mlibc/debug.hpp>
-#include <errno.h>
-#include <string.h>
-#include <stdlib.h>
 #include <poll.h>
-#include <sys/select.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/select.h>
 #include <sys/stat.h>
 #include <unistd.h>
-#include <dirent.h>
 
 #include <menix/syscall.h>
-#include <menix/archctl.h>
-#include <menix/powerctl.h>
 
 namespace mlibc {
-	void sys_libc_log(const char *message) {
-		ssize_t written;
-		sys_write(1, message, strlen(message), &written);
-		const char newline = '\n';
-		sys_write(1, &newline, 1, &written);
-	}
 
-	[[noreturn]] void sys_libc_panic() {
-		sys_exit(1);
-	}
+void sys_libc_log(const char *message) {
+	ssize_t written;
+	sys_write(1, message, strlen(message), &written);
+	const char newline = '\n';
+	sys_write(1, &newline, 1, &written);
+}
 
-	int sys_tcb_set(void *pointer) {
-		return syscall(SYSCALL_archctl, ArchCtl_SetFsBase, (size_t)pointer).error;
-	}
+[[noreturn]] void sys_libc_panic() {
+	sys_libc_log("mlibc panic!");
+	sys_exit(0xC);
+}
 
-	int sys_futex_tid() {
-		// TODO
-		return 0;
-	}
+void sys_exit(int code) {
+	syscall(SYSCALL_exit, code);
+	__builtin_unreachable();
+}
 
-	int sys_futex_wait(int *pointer, int expected, const struct timespec *time) {
-		// TODO
-		return 0;
-	}
+int sys_tcb_set(void *pointer) { return syscall(SYSCALL_savetls, (size_t)pointer).error; }
 
-	int sys_futex_wake(int *pointer) {
-		// TODO
-		return 0;
-	}
+int sys_futex_tid() { return syscall(SYSCALL_gettid).error; }
 
-	int sys_sigsuspend(const sigset_t *set) {
-		syscall(SYSCALL_sigsuspend, (size_t)set);
-		return EINTR;
-	}
-	
-	int sys_sigaction(int, const struct sigaction *__restrict, struct sigaction *__restrict) {
-		return 0;
-	}
+int sys_futex_wait(int *pointer, int expected, const struct timespec *time) {
+	return syscall(SYSCALL_futex_wait, (size_t)pointer, (size_t)expected, (size_t)time).error;
+}
 
-	int sys_anon_allocate(size_t size, void **pointer) {
-		syscall_result r = syscall(SYSCALL_mmap, 0, size, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, 0, 0);
-		SYSCALL_ERR_CHECK(r)
-		*pointer = (void*)r.value;
-		return 0;
-	}
+int sys_futex_wake(int *pointer) { return syscall(SYSCALL_futex_wake, (size_t)pointer).error; }
 
-	int sys_anon_free(void *pointer, size_t size) {
-		return syscall(SYSCALL_munmap, (size_t)pointer, size).error;
-	}
+int sys_sigsuspend(const sigset_t *set) {
+	syscall(SYSCALL_sigsuspend, (size_t)set);
+	return EINTR;
+}
 
-	int sys_open(const char *pathname, int flags, mode_t mode, int *fd) {
-		syscall_result r = syscall(SYSCALL_open, (size_t)pathname, flags, mode);
-		SYSCALL_ERR_CHECK(r)
-		*fd = (int)r.value;
-		return 0;
-	}
+int sys_sigaction(int, const struct sigaction *__restrict, struct sigaction *__restrict) {
+	return 0;
+}
 
-	int sys_read(int fd, void *buf, size_t count, ssize_t *bytes_read) {
-		syscall_result r = syscall(SYSCALL_read, (size_t)fd, (size_t)buf, count);
-		SYSCALL_ERR_CHECK(r)
-		*bytes_read = (ssize_t)r.value;
-		return 0;
-	}
+int sys_anon_allocate(size_t size, void **pointer) {
+	auto r =
+	    syscall(SYSCALL_mmap, 0, size, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, 0, 0);
+	if (r.error != 0)
+		return r.error;
+	*pointer = (void *)r.value;
+	return 0;
+}
 
-	int sys_seek(int fd, off_t offset, int whence, off_t *new_offset) {
-		syscall_result r = syscall(SYSCALL_seek, fd, offset, whence);
-		SYSCALL_ERR_CHECK(r)
-		*new_offset = r.value;
-		return 0;
-	}
+int sys_anon_free(void *pointer, size_t size) {
+	return syscall(SYSCALL_munmap, (size_t)pointer, size).error;
+}
 
-	int sys_close(int fd) {
-		return syscall(SYSCALL_close, fd).error;
-	}
+int sys_openat(int dirfd, const char *path, int flags, mode_t mode, int *fd) {
+	auto r = syscall(SYSCALL_openat, dirfd, (size_t)path, flags, mode);
+	if (r.error != 0)
+		return r.error;
+	*fd = (int)r.value;
+	return 0;
+}
 
-	int sys_stat(fsfd_target fsfdt, int fd, const char *path, int flags, struct stat *statbuf) {
-		return syscall(SYSCALL_stat, (size_t)fd, (size_t)path, (size_t)statbuf).error;
-	}
+int sys_open(const char *pathname, int flags, mode_t mode, int *fd) {
+	return sys_openat(AT_FDCWD, pathname, flags, mode, fd);
+}
 
-	int sys_vm_map(void *hint, size_t size, int prot, int flags, int fd, off_t offset, void **window) {
-		syscall_result r = syscall(SYSCALL_mmap, (size_t)hint, size, prot, flags, fd, offset);
-		SYSCALL_ERR_CHECK(r)
-		*window = (void*)r.value;
-		return 0;
-	}
+int sys_read(int fd, void *buf, size_t count, ssize_t *bytes_read) {
+	auto r = syscall(SYSCALL_read, (size_t)fd, (size_t)buf, count);
+	if (r.error != 0)
+		return r.error;
+	*bytes_read = (ssize_t)r.value;
+	return 0;
+}
 
-	int sys_vm_unmap(void *pointer, size_t size) {
-		return syscall(SYSCALL_munmap, (size_t)pointer, size).error;
-	}
+int sys_seek(int fd, off_t offset, int whence, off_t *new_offset) {
+	auto r = syscall(SYSCALL_seek, fd, offset, whence);
+	if (r.error != 0)
+		return r.error;
+	*new_offset = r.value;
+	return 0;
+}
 
-	int sys_vm_protect(void *pointer, size_t size, int prot) {
-		return syscall(SYSCALL_mprotect, (size_t)pointer, size, prot).error;
-	}
+int sys_close(int fd) { return syscall(SYSCALL_close, fd).error; }
 
-	void sys_exit(int code) {
-		syscall(SYSCALL_exit, code);
-		for (;;);
-	}
+int sys_stat(fsfd_target fsfdt, int fd, const char *path, int flags, struct stat *statbuf) {
+	(void)fsfdt;
+	return syscall(SYSCALL_stat, (size_t)fd, (size_t)path, flags, (size_t)statbuf).error;
+}
 
-	int sys_write(int fd, const void *buf, size_t count, ssize_t *bytes_written) {
-		syscall_result r = syscall(SYSCALL_write, (size_t)fd, (size_t)buf, count);
-		SYSCALL_ERR_CHECK(r)
-		*bytes_written = r.value;
-		return 0;
-	}
+int sys_vm_map(void *hint, size_t size, int prot, int flags, int fd, off_t offset, void **window) {
+	auto r = syscall(SYSCALL_mmap, (size_t)hint, size, prot, flags, fd, offset);
+	if (r.error != 0)
+		return r.error;
+	*window = (void *)r.value;
+	return 0;
+}
 
-	int sys_clock_get(int clock, time_t *secs, long *nanos) {
-		// TODO
-		return 0;
-	}
+int sys_vm_unmap(void *pointer, size_t size) {
+	return syscall(SYSCALL_munmap, (size_t)pointer, size).error;
+}
 
-	int sys_uname(struct utsname *buf) {
-		syscall_result r = syscall(SYSCALL_uname, (size_t)buf);
-		SYSCALL_ERR_CHECK(r)
-		return 0;
-	}
+int sys_vm_protect(void *pointer, size_t size, int prot) {
+	return syscall(SYSCALL_mprotect, (size_t)pointer, size, prot).error;
+}
 
-	int sys_fork(pid_t *child) {
-		syscall_result r = syscall(SYSCALL_fork);
-		SYSCALL_ERR_CHECK(r)
-		*child = r.value;
-		return 0;
-	}
+int sys_write(int fd, const void *buf, size_t count, ssize_t *bytes_written) {
+	auto r = syscall(SYSCALL_write, (size_t)fd, (size_t)buf, count);
+	if (r.error != 0)
+		return r.error;
+	*bytes_written = r.value;
+	return 0;
+}
 
-	int sys_waitpid(pid_t pid, int *status, int flags, struct rusage *ru, pid_t *ret_pid) {
-		syscall_result r = syscall(SYSCALL_waitpid, (size_t)pid, (size_t)status, flags);
-		SYSCALL_ERR_CHECK(r)
-		*ret_pid = (int)r.value;
-		return 0;
-	}
+int sys_clock_get(int clock, time_t *secs, long *nanos) {
+	struct timespec ts;
+	auto r = syscall(SYSCALL_readtimer, clock, (size_t)&ts);
+	if (r.error != 0)
+		return r.error;
+	*secs = ts.tv_sec;
+	*nanos = ts.tv_nsec;
+	return 0;
+}
 
-	int sys_chdir(const char *path) {
-		return syscall(SYSCALL_chdir, (size_t)path).error;
-	}
+int sys_uname(struct utsname *buf) { return syscall(SYSCALL_uname, (size_t)buf).error; }
 
-	pid_t sys_getpid() {
-		return syscall(SYSCALL_getpid).value;
-	}
+int sys_fork(pid_t *child) {
+	auto r = syscall(SYSCALL_fork);
+	if (r.error != 0)
+		return r.error;
+	*child = r.value;
+	return 0;
+}
 
-	pid_t sys_getppid() {
-		return 0;
-	}
+int sys_waitpid(pid_t pid, int *status, int flags, struct rusage *ru, pid_t *ret_pid) {
+	(void)ru;
+	auto r = syscall(SYSCALL_waitpid, (size_t)pid, (size_t)status, flags);
+	if (r.error != 0)
+		return r.error;
+	*ret_pid = (int)r.value;
+	return 0;
+}
 
-	uid_t sys_getuid() {
-		return 0;
-	}
+int sys_chdir(const char *path) { return syscall(SYSCALL_chdir, (size_t)path).error; }
 
-	uid_t sys_geteuid() {
-		return 0;
-	}
+pid_t sys_getpid() { return syscall(SYSCALL_getpid).value; }
 
-	gid_t sys_getgid() {
-		return 0;
-	}
+pid_t sys_getppid() { return 0; }
 
-	int sys_setgid(gid_t) {
-		return 0;
-	}
+uid_t sys_getuid() { return syscall(SYSCALL_getuid).value; }
 
-	int sys_getpgid(pid_t, pid_t *) {
-		return 0;
-	}
+uid_t sys_geteuid() { return 0; }
 
-	gid_t sys_getegid() {
-		return 0;
-	}
+gid_t sys_getgid() { return 0; }
 
-	int sys_setpgid(pid_t, pid_t) {
-		return 0;
-	}
+int sys_setgid(gid_t) { return 0; }
 
-	int sys_ttyname(int, char *, size_t) {
-		return ENOSYS;
-	}
+int sys_getpgid(pid_t, pid_t *) { return 0; }
 
-	int sys_getcwd(char *buffer, size_t size) {
-		return syscall(SYSCALL_getcwd, (size_t)buffer, size).error;
-	}
+gid_t sys_getegid() { return 0; }
 
-	int sys_execve(const char *path, char *const argv[], char *const envp[]) {
-		return syscall(SYSCALL_execve, (size_t)path, (size_t)argv, (size_t)envp).error;
-	}
+int sys_setpgid(pid_t, pid_t) { return 0; }
 
-	int sys_isatty(int fd) {
-		// TODO
-		if (fd >= 0 && fd < 2)
-			return 0;
-		return ENOTTY;
-	}
+int sys_ttyname(int, char *, size_t) { return ENOSYS; }
 
-	int sys_sigprocmask(int how, const sigset_t *__restrict set, sigset_t *__restrict retrieve) {
-		return 0;
-	}
+int sys_getcwd(char *buffer, size_t size) {
+	return syscall(SYSCALL_getcwd, (size_t)buffer, size).error;
+}
+
+int sys_execve(const char *path, char *const argv[], char *const envp[]) {
+	return syscall(SYSCALL_execve, (size_t)path, (size_t)argv, (size_t)envp).error;
+}
+
+int sys_isatty(int fd) { return syscall(SYSCALL_isatty, fd).value; }
+
+int sys_sigprocmask(int how, const sigset_t *__restrict set, sigset_t *__restrict retrieve) {
+	return syscall(SYSCALL_sigprocmask, how, (size_t)set, (size_t)retrieve).error;
+}
 
 } // namespace mlibc
--- sysdeps/menix/include/menix/syscall.h
+++ sysdeps/menix/include/menix/syscall.h
@@ -4,49 +4,71 @@
 #include <stddef.h>
 
 #define SYSCALL_exit 0
-#define SYSCALL_open 1
-#define SYSCALL_close 2
-#define SYSCALL_stat 3
-#define SYSCALL_access 4
-#define SYSCALL_read 5
-#define SYSCALL_write 6
-#define SYSCALL_seek 7
-#define SYSCALL_ioctl 8
-#define SYSCALL_chdir 9
-#define SYSCALL_chmod 10
-#define SYSCALL_chown 11
-#define SYSCALL_mount 12
-#define SYSCALL_unmount 13
-#define SYSCALL_link 14
-#define SYSCALL_unlink 15
-#define SYSCALL_symlink 16
-#define SYSCALL_readlink 17
-#define SYSCALL_rmdir 18
-#define SYSCALL_setuid 19
-#define SYSCALL_getuid 20
-#define SYSCALL_setgid 21
-#define SYSCALL_getgid 22
-#define SYSCALL_sync 23
-#define SYSCALL_mmap 24
-#define SYSCALL_munmap 25
-#define SYSCALL_mremap 26
-#define SYSCALL_mprotect 27
-#define SYSCALL_execve 28
-#define SYSCALL_fork 29
-#define SYSCALL_kill 30
-#define SYSCALL_getpid 31
-#define SYSCALL_getparentpid 32
-#define SYSCALL_recvmsg 33
-#define SYSCALL_sendmsg 34
-#define SYSCALL_accept 35
-#define SYSCALL_getpeername 36
-#define SYSCALL_getsockname 37
-#define SYSCALL_uname 38
-#define SYSCALL_powerctl 39
-#define SYSCALL_archctl 40
-#define SYSCALL_getcwd 41
-#define SYSCALL_waitpid 42
-#define SYSCALL_sigsuspend 43
+#define SYSCALL_sigprocmask 1
+#define SYSCALL_sigsuspend 2
+#define SYSCALL_sigpending 3
+#define SYSCALL_sigaction 4
+#define SYSCALL_sigreturn 5
+#define SYSCALL_sigtimedwait 6
+#define SYSCALL_mmap 7
+#define SYSCALL_munmap 8
+#define SYSCALL_mremap 9
+#define SYSCALL_mprotect 10
+#define SYSCALL_execve 11
+#define SYSCALL_fork 12
+#define SYSCALL_kill 13
+#define SYSCALL_gettid 14
+#define SYSCALL_getpid 15
+#define SYSCALL_waitpid 16
+#define SYSCALL_read 17
+#define SYSCALL_write 18
+#define SYSCALL_seek 19
+#define SYSCALL_ioctl 20
+#define SYSCALL_openat 21
+#define SYSCALL_close 22
+#define SYSCALL_stat 23
+#define SYSCALL_faccessat 24
+#define SYSCALL_fcntl 25
+#define SYSCALL_readdir 26
+#define SYSCALL_getcwd 27
+#define SYSCALL_chdir 28
+#define SYSCALL_fchdir 29
+#define SYSCALL_mkdirat 30
+#define SYSCALL_rename 31
+#define SYSCALL_chmodat 32
+#define SYSCALL_chownat 33
+#define SYSCALL_linkat 34
+#define SYSCALL_unlinkat 35
+#define SYSCALL_readlinkat 36
+#define SYSCALL_mount 37
+#define SYSCALL_unmount 38
+#define SYSCALL_setuid 39
+#define SYSCALL_getuid 40
+#define SYSCALL_setgid 41
+#define SYSCALL_getgid 42
+#define SYSCALL_umask 43
+#define SYSCALL_poll 44
+#define SYSCALL_isatty 45
+#define SYSCALL_chroot 46
+#define SYSCALL_futex_wait 47
+#define SYSCALL_futex_wake 48
+#define SYSCALL_socket 49
+#define SYSCALL_socketpair 50
+#define SYSCALL_bind 51
+#define SYSCALL_connect 52
+#define SYSCALL_accept 53
+#define SYSCALL_listen 54
+#define SYSCALL_getpeername 55
+#define SYSCALL_getsockname 56
+#define SYSCALL_getsockopt 57
+#define SYSCALL_setsockopt 58
+#define SYSCALL_recvmsg 59
+#define SYSCALL_sendmsg 60
+#define SYSCALL_sethostname 61
+#define SYSCALL_uname 62
+#define SYSCALL_archctl 63
+#define SYSCALL_readtimer 64
+#define SYSCALL_savetls 65
 
 #ifndef __MLIBC_ABI_ONLY
 
@@ -56,26 +78,30 @@ struct syscall_result {
 };
 static_assert(sizeof(syscall_result) == 16);
 
-#define SYSCALL_ERR_CHECK(result) if (result.error != 0) { return result.error; }
-
 #ifdef __x86_64__
-extern "C" inline syscall_result syscall(size_t num, size_t a0 = 0, size_t a1 = 0, size_t a2 = 0, size_t a3 = 0, size_t a4 = 0, size_t a5 = 0) {
+extern "C" inline syscall_result syscall(
+    size_t num,
+    size_t a0 = 0,
+    size_t a1 = 0,
+    size_t a2 = 0,
+    size_t a3 = 0,
+    size_t a4 = 0,
+    size_t a5 = 0
+) {
 	syscall_result r;
-	asm volatile(
-		"mov %2, %%rax;"
-		"mov %3, %%rdi;"
-		"mov %4, %%rsi;"
-		"mov %5, %%rdx;"
-		"mov %6, %%r8;"
-		"mov %7, %%r10;"
-		"mov %8, %%r9;"
-		"syscall;"
-		"mov %%rax, %0;"
-		"mov %%rdx, %1;"
-		: "=g"(r.value), "=g"(r.error)
-		: "g"(num), "g"(a0), "g"(a1), "g"(a2), "g"(a3), "g"(a4), "g"(a5)
-		: "rax", "rdi", "rsi", "rdx", "r8", "r10", "r9", "rcx", "r11", "memory"
-	);
+	asm volatile("mov %2, %%rax;"
+	             "mov %3, %%rdi;"
+	             "mov %4, %%rsi;"
+	             "mov %5, %%rdx;"
+	             "mov %6, %%r8;"
+	             "mov %7, %%r10;"
+	             "mov %8, %%r9;"
+	             "syscall;"
+	             "mov %%rax, %0;"
+	             "mov %%rdx, %1;"
+	             : "=g"(r.value), "=g"(r.error)
+	             : "g"(num), "g"(a0), "g"(a1), "g"(a2), "g"(a3), "g"(a4), "g"(a5)
+	             : "rax", "rdi", "rsi", "rdx", "r8", "r10", "r9", "rcx", "r11", "memory");
 	return r;
 }
 #endif /* __x86_64 */
